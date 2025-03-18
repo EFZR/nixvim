56b4df7c6113d4a02c4d9123f0073b9a6f96688b
efzr <emerson.zapatarivas@gmail.com>
2025-03-18 09:09:02 -0600

    Fix: Vue auto tag html

--- nvim-configs/ts-autotag.nix
+++ nvim-configs/ts-autotag.nix
@@ -3,24 +3,10 @@
     ts-autotag = {
       enable = true;
       settings = {
-        aliases = {
-          astro = "html";
-          blade = "html";
-          eruby = "html";
-          handlebars = "glimmer";
-          hbs = "glimmer";
-          htmldjango = "html";
-          javascript = "typescriptreact";
-          "javascript.jsx" = "typescriptreact";
-          javascriptreact = "typescriptreact";
-          markdown = "html";
-          php = "html";
-          rescript = "typescriptreact";
-          rust = "rust";
-          twig = "html";
-          typescript = "typescriptreact";
-          "typescript.tsx" = "typescriptreact";
-          vue = "html";
+        per_filetype = {
+          vue = {
+            enable_close = true;
+          };
         };
       };
     };
