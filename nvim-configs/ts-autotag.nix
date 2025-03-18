a0531f3cb8de84baf32b95e8c57105b3c41186ee
efzr <emerson.zapatarivas@gmail.com>
2025-03-18 08:58:08 -0600

    Feat: ts-autotag with vue

--- nvim-configs/ts-autotag.nix
+++ nvim-configs/ts-autotag.nix
@@ -2,6 +2,27 @@
   plugins = {
     ts-autotag = {
       enable = true;
+      settings = {
+        aliases = {
+          astro = "html";
+          blade = "html";
+          eruby = "html";
+          handlebars = "glimmer";
+          hbs = "glimmer";
+          htmldjango = "html";
+          javascript = "typescriptreact";
+          "javascript.jsx" = "typescriptreact";
+          javascriptreact = "typescriptreact";
+          markdown = "html";
+          php = "html";
+          rescript = "typescriptreact";
+          rust = "rust";
+          twig = "html";
+          typescript = "typescriptreact";
+          "typescript.tsx" = "typescriptreact";
+          vue = "html";
+        };
+      };
     };
   };
 }
